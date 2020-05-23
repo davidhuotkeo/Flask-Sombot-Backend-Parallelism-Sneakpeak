2985a303897e8548e1f97321b024658fea1c023c
David <david@Davids-MacBook-Air.local>
2020-05-23 09:41:12 +0700

    commiting all file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Parallelism in Flask Application
+Sending smtp relay is a pain in the ass when it comes to waiting.
+I decided to create a async await in python to finish after the
+http request of the user, which optimizes the user experience to
+another whole new level
+
+## File structuring
+In this backend application, I use MVC (Model View Controller) framework,
+which can be seen in file_structure.txt
---
+++ email_parallelism.py
@@ -0,0 +1,28 @@
+@event.route("/event/send/<eventid>")
+def send_ticket(eventid):
+    # Query event
+    event = Event.query.filter(Event.id == eventid).first()
+
+    # get the audience relationship
+    audiences = event.audience
+
+    # if there is no audience uploaded response 404
+    if not audiences:
+        return jsonify({"sent": None}), 404
+
+    # create the list and some variables for send_email func kwargs
+    audience_list_object = []
+    subject = f"Digital Ticket from {event.name} Event"
+    msg = event.body
+
+    # load the Audience object and append to the list
+    for people in audiences:
+        identity = people.id
+        email = people.email
+        audience_list_object.append(AudienceEmail(identity, email))
+
+    # Parallelism sending email
+    email_arguments = {"audiences": audience_list_object, "subject": subject, "message": msg}
+    email_parallel = Thread(target=send_email, kwargs=email_arguments)
+
+    return jsonify({"sent": True})
